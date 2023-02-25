b7d4f57c1ca1dc507ffbcdea4ad77483a743f4f2
Madi015 <madisaed2003@yahoo.com>
2023-02-25 14:05:14 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D15_inlämning__objekt_och_git_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
